852c10a1fde5cf25830eddb6876914b60ba1c26c
drivebyer <wuyangmuc@163.com>
2021-11-18 18:54:58 +0800

    mod: import SQLE plugin adaptor

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/actiontech/sqle-oracle-plugin
+
+go 1.16
+
+require (
+	github.com/actiontech/sqle v1.2111.0-pre2.0.20211118101025-1f63ff3d029a
+	github.com/denisenkom/go-mssqldb v0.11.0 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/golang/glog v1.0.0 // indirect
+	github.com/hashicorp/go-hclog v1.0.0 // indirect
+	github.com/hashicorp/go-plugin v1.4.3 // indirect
+	github.com/hashicorp/yamux v0.0.0-20211028200310-0bc27b27de87 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
+	github.com/oklog/run v1.1.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
+	golang.org/x/net v0.0.0-20211116231205-47ca1ff31462 // indirect
+	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
+	google.golang.org/genproto v0.0.0-20211117155847-120650a500bb // indirect
+	google.golang.org/grpc v1.42.0 // indirect
+)
---
+++ vendor/github.com/actiontech/sqle/sqle/driver/driver.go
@@ -0,0 +1,304 @@
+package driver
+
+import (
+	"context"
+	"database/sql/driver"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	// drivers store instantiate handlers for MySQL or gRPC plugin.
+	drivers   = make(map[string]handler)
+	driversMu sync.RWMutex
+
+	// rules store audit rules for each driver.
+	rules   map[string][]*Rule
+	rulesMu sync.RWMutex
+)
+
+const (
+	SQLTypeDML = "dml"
+	SQLTypeDDL = "ddl"
+)
+
+const (
+	DriverTypeMySQL      = "mysql"
+	DriverTypePostgreSQL = "PostgreSQL"
+)
+
+// DSN provide necessary information to connect to database.
+type DSN struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+
+	// DatabaseName is the default database to connect.
+	DatabaseName string
+}
+
+type RuleLevel string
+
+const (
+	RuleLevelNormal RuleLevel = "normal"
+	RuleLevelNotice RuleLevel = "notice"
+	RuleLevelWarn   RuleLevel = "warn"
+	RuleLevelError  RuleLevel = "error"
+)
+
+var ruleLevelMap = map[RuleLevel]int{
+	RuleLevelNormal: 0,
+	RuleLevelNotice: 1,
+	RuleLevelWarn:   2,
+	RuleLevelError:  3,
+}
+
+type Rule struct {
+	Name string
+	Desc string
+
+	// Category is the category of the rule. Such as "Naming Conventions"...
+	// Rules will be displayed on the SQLE rule list page by category.
+	Category string
+
+	Level RuleLevel
+	Value string
+}
+
+func (r *Rule) GetValueInt(defaultRule *Rule) int64 {
+	value := r.GetValue()
+	i, err := strconv.ParseInt(value, 10, 64)
+	if err == nil {
+		return i
+	}
+	i, err = strconv.ParseInt(defaultRule.GetValue(), 10, 64)
+	if err == nil {
+		return i
+	}
+	return 0
+}
+
+func (r *Rule) GetValue() string {
+	if r == nil {
+		return ""
+	}
+	return r.Value
+}
+
+// Config difine the configuration for driver.
+type Config struct {
+	DSN   *DSN
+	Rules []*Rule
+}
+
+// NewConfig return a config for driver.
+//
+// 1. dsn is nil, rules is not nil. Use drive to do Offline Audit.
+// 2. dsn is not nil, rule is nil. Use drive to communicate with databse only.
+// 3. dsn is not nil, rule is not nil. Most common usecase.
+func NewConfig(dsn *DSN, rules []*Rule) (*Config, error) {
+	if dsn == nil && rules == nil {
+		fmt.Println("dsn is nil, and rules is nil, nothing can be done by driver")
+	}
+
+	return &Config{
+		DSN:   dsn,
+		Rules: rules,
+	}, nil
+}
+
+// handler is a template which Driver plugin should provide such function signature.
+type handler func(log *logrus.Entry, c *Config) (Driver, error)
+
+// Register like sql.Register.
+//
+// Register makes a database driver available by the provided driver name.
+// Driver's initialize handler and audit rules register by Register.
+func Register(name string, h handler, rs []*Rule) {
+	_, exist := drivers[name]
+	if exist {
+		panic("duplicated driver name")
+	}
+
+	driversMu.Lock()
+	drivers[name] = h
+	driversMu.Unlock()
+
+	rulesMu.Lock()
+	if rules == nil {
+		rules = make(map[string][]*Rule)
+	}
+	rules[name] = rs
+	rulesMu.Unlock()
+}
+
+type ErrDriverNotSupported struct {
+	DriverTyp string
+}
+
+func (e *ErrDriverNotSupported) Error() string {
+	return fmt.Sprintf("driver type %v is not supported", e.DriverTyp)
+}
+
+// NewDriver return a new instantiated Driver.
+func NewDriver(log *logrus.Entry, dbType string, cfg *Config) (Driver, error) {
+	driversMu.RLock()
+	defer driversMu.RUnlock()
+
+	d, exist := drivers[dbType]
+	if !exist {
+		return nil, fmt.Errorf("driver type %v is not supported", dbType)
+	}
+
+	return d(log, cfg)
+}
+
+func AllRules() map[string][]*Rule {
+	rulesMu.RLock()
+	defer rulesMu.RUnlock()
+	return rules
+}
+
+func AllDrivers() []string {
+	rulesMu.RLock()
+	defer rulesMu.RUnlock()
+
+	driverNames := make([]string, 0, len(drivers))
+	for n := range drivers {
+		driverNames = append(driverNames, n)
+	}
+	return driverNames
+}
+
+var ErrNodesCountExceedOne = errors.New("after parse, nodes count exceed one")
+
+// Driver is a interface that must be implemented by a database.
+//
+// It's implementation maybe on the same process or over gRPC(by go-plugin).
+//
+// Driver is responsible for two primary things:
+// 1. privode handle to communicate with database
+// 2. audit SQL with rules
+type Driver interface {
+	Close(ctx context.Context)
+	Ping(ctx context.Context) error
+	Exec(ctx context.Context, query string) (driver.Result, error)
+	Tx(ctx context.Context, queries ...string) ([]driver.Result, error)
+
+	// Schemas export all supported schemas.
+	//
+	// For example, performance_schema/performance_schema... which in MySQL is not allowed for auditing.
+	Schemas(ctx context.Context) ([]string, error)
+
+	// Parse parse sqlText to Node array.
+	//
+	// sqlText may be single SQL or batch SQLs.
+	Parse(ctx context.Context, sqlText string) ([]Node, error)
+
+	// Audit sql with rules. sql is single SQL text.
+	//
+	// Multi Audit call may be in one context.
+	// For example:
+	//		driver, _ := NewDriver(..., ..., ...)
+	// 		driver.Audit(..., "CREATE TABLE t1(id int)")
+	// 		driver.Audit(..., "SELECT * FROM t1 WHERE id = 1")
+	//      ...
+	// driver should keep SQL context during it's lifecycle.
+	Audit(ctx context.Context, sql string) (*AuditResult, error)
+
+	// GenRollbackSQL generate sql's rollback SQL.
+	GenRollbackSQL(ctx context.Context, sql string) (string, string, error)
+}
+
+// Registerer is the interface that all SQLe plugins must support.
+type Registerer interface {
+	// Name returns plugin name.
+	Name() string
+
+	// Rules returns all rules that plugin supported.
+	Rules() []*Rule
+}
+
+// Node is a interface which unify SQL ast tree. It produce by Driver.Parse.
+type Node struct {
+	// Text is the raw SQL text of Node.
+	Text string
+
+	// Type is type of SQL, such as DML/DDL/DCL.
+	Type string
+
+	// Fingerprint is fingerprint of Node's raw SQL.
+	Fingerprint string
+}
+
+// // DSN like https://github.com/go-sql-driver/mysql/blob/master/dsn.go. type Config struct
+// type DSN struct {
+// 	Type string
+
+// 	Host   string
+// 	Port   string
+// 	User   string
+// 	Pass   string
+// 	DBName string
+// }
+
+type AuditResult struct {
+	results []*auditResult
+}
+
+type auditResult struct {
+	level   RuleLevel
+	message string
+}
+
+func NewInspectResults() *AuditResult {
+	return &AuditResult{
+		results: []*auditResult{},
+	}
+}
+
+// Level find highest Level in result
+func (rs *AuditResult) Level() RuleLevel {
+	level := RuleLevelNormal
+	for _, curr := range rs.results {
+		if ruleLevelMap[curr.level] > ruleLevelMap[level] {
+			level = curr.level
+		}
+	}
+	return level
+}
+
+func (rs *AuditResult) Message() string {
+	messages := make([]string, len(rs.results))
+	for n, result := range rs.results {
+		var message string
+		match, _ := regexp.MatchString(fmt.Sprintf(`^\[%s|%s|%s|%s|%s\]`,
+			RuleLevelError, RuleLevelWarn, RuleLevelNotice, RuleLevelNormal, "osc"),
+			result.message)
+		if match {
+			message = result.message
+		} else {
+			message = fmt.Sprintf("[%s]%s", result.level, result.message)
+		}
+		messages[n] = message
+	}
+	return strings.Join(messages, "\n")
+}
+
+func (rs *AuditResult) Add(level RuleLevel, message string, args ...interface{}) {
+	if level == "" || message == "" {
+		return
+	}
+
+	rs.results = append(rs.results, &auditResult{
+		level:   level,
+		message: fmt.Sprintf(message, args...),
+	})
+}
